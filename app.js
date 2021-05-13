7f9e4a6b53a7f4111e621f7d5bf49fda070ce8df
Sourav Singh <souravsingh821.ss@gmail.com>
2021-05-13 11:51:15 +0530

    Add Enviroment Vars

---
+++ app.js
@@ -0,0 +1,81 @@
+//jshint esversion:6
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+
+app.use(express.static('Public'));
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+mongoose.connect("mongodb://localhost:27017/userDB",{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    password:String
+});
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt,{secret:secret,encryptedFields:['password']});
+
+const User = new mongoose.model('User',userSchema);
+
+//Router
+//*Get 
+app.get('/',(req,res)=>{
+    res.render('home')
+});
+
+app.get('/login',(req,res)=>{
+    res.render('login')
+});
+
+app.get('/register',(req,res)=>{
+    res.render('register')
+});
+
+//*POST
+app.post('/register',(req,res)=>{
+    const newUser = new User({
+        email:req.body.username,
+        password:req.body.password
+    })
+    newUser.save((err)=>{
+        if(err)
+        console.log(err);
+        else
+        res.render('secrets');
+    });
+})
+
+app.post('/login',(req,res)=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({email:username},(err,found)=>{
+        if(err){
+            console.log(err);
+        }
+        else{
+            if(found){
+                if(found.password === password){
+                    res.render('secrets');
+                }
+            }
+        }
+    });
+});
+
+app.listen(3000,()=>{
+    console.log("server is started on part 3000");
+});
